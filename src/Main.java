74ed779191049359e17402d14fd8a90faaf4f5c2
” <827851647@qq.com>
2020-02-01 14:42:36 +0800

    pop

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        int[] a={3,5,9,4,6,7,1,0,5};
+        pop.laobiao1pop(a);
+        for (int i = 0; i <a.length ; i++) {
+            System.out.print(a[i]);
+        }
+
+    }
+}
---
+++ src/pop.java
@@ -0,0 +1,16 @@
+class pop{
+    //冒泡排序
+    public static void laobiao1pop(int[] a){
+
+        for (int i = 0; i < a.length-1; i++) {
+            for (int j = 0; j <a.length-i-1 ; j++) {
+                if (a[j]>a[j+1])
+                {
+                    int temp=a[j+1];
+                    a[j+1]=a[j];
+                    a[j]=temp;
+                }
+            }
+        }
+    }
+}
